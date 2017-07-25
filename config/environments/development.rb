8d058c6cb0df76c82ebe3bd3c84dff218d8c8506
Yuta <ny_bigapplejr@yahoo.co.jp>
2017-07-25 11:07:06 +0000

    201707252007

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'ide.c9.io/naghoge/oyama-yuta/' }
+  config.action_mailer.default_url_options = { host: 'https://ide.c9.io/naghoge/oyama-yuta' }
   config.action_mailer.delivery_method = :letter_opener_web
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
